9b68b28decc0e8775a4a9cdc2635a790563a2203
Yeshwanth D <166380375+YeshwanthD75@users.noreply.github.com>
2024-10-17 09:51:58 +0530

    next.js files commited successfully

---
+++ frontend/pages/index.js
@@ -0,0 +1,21 @@
+// pages/index.js
+import Head from 'next/head';
+import Chatbot from '../components/Chatbot';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Gen AIus KT - Chatbot Interface</title>
+        <meta name="description" content="Gen AIus KT - A professional chatbot interface" />
+        <link rel="icon" />
+      </Head>
+
+      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '1px' }}>
+        
+        <Chatbot />
+      </main>
+
+    </div>
+  );
+}
